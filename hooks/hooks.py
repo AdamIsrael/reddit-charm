332be8465ffd36b8b3f329e8fecd384f02b75be2
Adam Israel <adam@adamisrael.com>
2014-07-15 01:12:42 -0400

    setup users; git pull from all reddit repos

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 
 import os
+import os.path
 import sys
 import subprocess
+import shutil, glob
 
 sys.path.insert(0, os.path.join(os.environ['CHARM_DIR'], 'lib'))
 
 from charmhelpers.core import (
-    hookenv,
-    host,
+    hookenv
+    , host
 )
 
 from charmhelpers.fetch import (
-    add_source,
-    apt_update,
-    apt_install
+    add_source
+    , apt_update
+    , apt_install
 )
 
 from charmhelpers.core.hookenv import (
@@ -26,6 +28,9 @@ log = hookenv.log
 
 
 SERVICE = 'reddit'
+REDDIT_USER = 'reddit'
+REDDIT_GROUP = 'reddit'
+REDDIT_HOME = '/home/reddit'
 
 PACKAGES = [
     'netcat-openbsd'
@@ -94,7 +99,25 @@ def install():
     
     # Install modules via pip that aren't available via apt-get:
     pip_install (PIP_MODULES)
-        
+    
+    add_user_group()
+    
+    git_pull()
+    
+    # os.chmod(new_mongo_dir, curr_dir_stat.st_mode)    
+    cmd = ['chown', '-R', '%s:%s' % (REDDIT_USER, REDDIT_GROUP), REDDIT_HOME]
+    print cmd
+    subprocess.call(cmd)
+    
+    log('Configuring Cassandra')
+    
+    log('Configuring PostgreSQL')
+    
+    log('Configuring RabbitMQ')
+    
+    log('Installing Reddit')
+    
+    
     return True
 
 
@@ -127,6 +150,78 @@ def stop():
     #host.service_stop(SERVICE)
     None
 
+
+# function clone_reddit_repo {
+#     local destination=$REDDIT_HOME/src/${1}
+#     local repository_url=https://github.com/${2}.git
+#
+#     if [ ! -d $destination ]; then
+#         sudo -u $REDDIT_USER git clone $repository_url $destination
+#     fi
+#
+#     if [ -d $destination/upstart ]; then
+#         cp $destination/upstart/* /etc/init/
+#     fi
+# }
+# clone_reddit_repo reddit reddit/reddit
+def clone_reddit_repo (target, repo):
+    repository_url = 'https://github.com/%s.git' % repo
+    destination = '%s/src/%s' % (REDDIT_HOME, target)
+    
+    log('Cloning github repo %s to %s' % (repo, destination))
+
+    if os.path.isdir(destination) is False:
+        # mkdir
+        host.mkdir(destination)
+        
+        cmd = ['git', 'clone', repository_url, destination]
+        print cmd
+        subprocess.call(cmd)
+
+        # if [ -d $destination/upstart ]; then
+        #     cp $destination/upstart/* /etc/init/
+        # fi
+        
+        if os.path.isdir('%s/upstart' % destination):
+            log('Copying upstart script(s)')
+            #/home/reddit/src/reddit/upstart
+            #/home/reddit/src/reddit/upstart/*
+            for file in glob.glob('%s/upstart/*' % destination):
+                shutil.copy (file, '/etc/init/')
+        # cmd = ['chown', '-R', '%s:%s' % (REDDIT_USER, REDDIT_GROUP), destination]
+        # print cmd
+        # subprocess.call(cmd)
+        
+    else:
+        # chdir
+        # git pull
+        # cmd = ['sudo', '-u %s' % REDDIT_USER, 'git', 'clone', GIT_URL, srcdir]
+        # log(cmd)
+        # subprocess.call(cmd_line)
+        None
+
+def clone_reddit_plugin_repo (name):
+    clone_reddit_repo (name, 'reddit/reddit-plugin-%s' % name)
+    
+def git_pull():
+    #log('Cloning %s' % GIT_URL)
+
+    # clone_reddit_repo reddit reddit/reddit
+    # clone_reddit_repo i18n reddit/reddit-i18n
+    # clone_reddit_plugin_repo about
+    # clone_reddit_plugin_repo liveupdate
+    # clone_reddit_plugin_repo meatspace
+
+    clone_reddit_repo('reddit', 'reddit/reddit')
+    clone_reddit_repo('i18n', 'reddit/reddit-i18n')
+    clone_reddit_plugin_repo('about')
+    clone_reddit_plugin_repo('liveupdate')
+    clone_reddit_plugin_repo('meatspace')
+        
+def add_user_group():
+    host.adduser(REDDIT_USER)
+    host.add_user_to_group(REDDIT_USER, REDDIT_GROUP)
+    
 def pip_install(packages=None, upgrade=False):
     # Build in /tmp or Juju's internal git will be confused
     cmd_line = ['pip', 'install', '--src', '/usr/src/']
