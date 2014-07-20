8b26f8a44d8cc840f8d17a4f42f9c5740485ea98
Adam Israel <adam@adamisrael.com>
2014-07-19 21:39:02 -0400

    Flesh out readme

--- README.ex
+++ README.ex
@@ -1,32 +1,31 @@
 # Overview
 
-Describe the intended usage of this charm and anything unique about how this charm relates to others here.
-
-This README will be displayed in the Charm Store, it should be either Markdown or RST. Ideal READMEs include instructions on how to use the charm, expected usage, and charm features that your audience might be interested in. For an example of a well written README check out Hadoop: http://jujucharms.com/charms/precise/hadoop
-
-Use this as a Markdown reference if you need help with the formatting of this README: http://askubuntu.com/editing-help
-
-This charm provides [service](http://example.com). Add a description here of what the service itself actually does.
+This reddit charm provides the means to get a working version of [reddit](https://github.com/reddit/reddit).
 
 Also remember to check the [icon guidelines](https://juju.ubuntu.com/docs/authors-charm-icon.html) so that your charm looks good in the Juju GUI.
 
 # Usage
 
-Step by step instructions on using the charm:
+In order to deploy this charm, you will need a working juju installation. Once bootstrapped, issue these commands:
 
-    juju deploy servicename
+    juju deploy cassandra
+    juju deploy postgresql
+    juju deploy rabbitmq-server
+    juju deploy memcached
+    juju deploy reddit
+    juju add-relation reddit:database cassandra:database
+    juju add-relation reddit postgresql
+    juju add-relation reddit rabbitmq-server
+    juju add-relation reddit memcached    
+    juju deploy gunicorn
+    juju add-relation reddit gunicorn    
+    juju expose reddit
 
-and so on. If you're providing a web service or something that the end user needs to go to, tell them here, especially if you're deploying a service that might listen to a non-default port.
+After a successful deployment, you can get the reddit unit IP address with:
 
-You can then browse to http://ip-address to configure the service.
+    juju status reddit
 
-## Scale out Usage
-
-If the charm has any recommendations for running at scale, outline them in examples here. For example if you have a memcached relation that improves performance, mention it here.
-
-## Known Limitations and Issues
-
-This not only helps users but gives people a place to start if they want to help you add features to your charm.
+and then browse to http://ip-address to configure the service. The source files are installed to /home/reddit/src/reddit, in the unit machine's file system.
 
 # Configuration
 
@@ -34,11 +33,10 @@ The configuration options will be listed on the charm store, however If you're m
 
 # Contact Information
 
-Though this will be listed in the charm store itself don't assume a user will know that, so include that information here:
+Charm maintainer: Adam Israel
 
 ## Upstream Project Name
 
-- Upstream website
+- Upstream website(https://github.com/reddit/reddit)
 - Upstream bug tracker
 - Upstream mailing list or contact information
-- Feel free to add things if it's useful for users
